29410d677a7576bdbecc6dfbbacac3cee02697c1
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-24 18:32:19 +0800

    updated files

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "availability_zones" {
-  value = data.aws_availability_zones.available.names
+output "vpc1_id" {
+  value = module.vpc1.vpc_id
+}
+
+output "vpc2_id" {
+  value = module.vpc2.vpc_id
 }
--- vpc-peering.tf
+++ vpc-peering.tf
@@ -1,6 +1,10 @@
 # Create VPC peering connection
 resource "aws_vpc_peering_connection" "peering" {
-  vpc_id = module.vpc1.default_vpc_id  # Specify requester VPC
-  peer_vpc_id = module.vpc2.default_vpc_id # Specify accepter VPC
+  vpc_id = module.vpc1.vpc_id  # Specify requester VPC
+  peer_vpc_id = module.vpc2.vpc_id # Specify accepter VPC
   auto_accept = true  # Specify if the peering connection should be automatically accepted
+
+  tags = {
+    Name = "yap-vpc-peering"
+  }
 }
